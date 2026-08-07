761b94685821bee10e14104f7bf2179c590255c5
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-08-07 16:46:08 +0300

    Update go.mod & go.sum, realized collect metrics function

--- app.go
+++ app.go
@@ -3,11 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
+	//"runtime/metrics"
+	"sync"
+	"time"
+	"log"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/northwindlight/cputemp"
 )
+
+type SliceMetrics struct {
+	CheckTime time.Time `json:"check_time"`
+	Metrics []Metric `json:"metrics"`
+}
+
+type Metric struct {
+	Name string `json:"name"`
+	Value float64 `json:"value"`
+}
 
 // App struct
 type App struct {
 	ctx context.Context
+	metrics []SliceMetrics
+	mu sync.Mutex
 }
 
 // NewApp creates a new App application struct
@@ -25,3 +46,41 @@ func (a *App) startup(ctx context.Context) {
 func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
+
+func (a *App) CollectMetrics() {
+	var collected SliceMetrics
+
+	vMem, err := mem.VirtualMemory()
+	if err != nil {
+		log.Printf("RAM scanning error: %v", err)
+	} else {
+		collected.Metrics = append(collected.Metrics, Metric{Name: "RAM", Value: vMem.UsedPercent})
+	}
+	cpuPercents, err := cpu.Percent(0, false)
+	if err != nil || len(cpuPercents) == 0 {
+		log.Printf("CPU scanning error: %v", err)
+	} else {
+		collected.Metrics = append(collected.Metrics, Metric{Name: "CPU", Value: cpuPercents[0]})
+	}
+	usage, err := disk.Usage("/")
+	if err != nil {
+		log.Printf("Disk scanning error: %v", err)
+	} else {
+		collected.Metrics = append(collected.Metrics, Metric{Name: "DISK", Value: usage.UsedPercent})
+	}
+	temp, err := cputemp.GetCPUTemperature()
+	if err != nil {
+		log.Printf("Temperature scanning error: %v", err)
+	} else {
+		collected.Metrics = append(collected.Metrics, Metric{Name: "TEMP", Value: temp})
+	}
+
+	a.mu.Lock()
+	a.metrics = append(a.metrics, collected)
+	if len(a.metrics) > 100 {
+		a.metrics = a.metrics[1:]
+	}
+	a.mu.Unlock()
+}
+
+
